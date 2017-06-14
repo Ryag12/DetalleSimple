f4436fd1e53a53524be00a3a630a0843762071e4
Roberti Amarante <yavier.ag@gmail.com>
2017-06-14 16:30:53 -0400

    .....

---
+++ DetalleSimple/Models/Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DetalleSimple.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public double Precio { get; set; }
+
+        public int Existencia { get; set; }
+    }
+}
---
+++ DetalleSimple/Models/Cotizaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DetalleSimple.Models
+{
+    public class Cotizaciones
+    {
+        [Key]
+        public int CotizacionId { get; set; }
+
+        public string Cliente { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public double Monto { get; set; }
+
+        public Cotizaciones()
+        {
+
+        }
+    }
+}
